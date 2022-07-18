eb5c11b2f17336331825a9b3082781887062a8f5
Mehran Tarif <mrmntf@gmail.com>
2022-07-18 16:21:15 +0430

    lesson 6

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-course
+## Lesson 6
---
+++ hello.py
@@ -0,0 +1,16 @@
+x = 10.0
+y = 10
+
+print(type(x))
+print(type(y))
+
+x = "10"
+y = 10
+
+print(type(x))
+print(type(y))
+
+students = ["Mehran", "Sara", "Ali"]
+print(type(students))
+
+print(type(10))
